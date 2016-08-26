1720159df12f16fd79812657d435d0a0af21b8d8
Seiji Fujita <SeijiFujita0420@gmail.com>
2016-08-27 06:20:54 +0900

    benchmark_dmd_ldc add benchmark()

---
+++ benchmark_dmd_ldc/Build.bat
@@ -0,0 +1,101 @@
+@echo off
+rem ---- DMD path
+path=C:\D\dmd.2.071.1\windows\bin;
+
+dmd -O -inline -release -boundscheck=off -m32 -ofbenchmark32.exe benchmark.d
+@if ERRORLEVEL 1 goto :eof
+benchmark32.exe
+
+dmd -O -inline -release -boundscheck=off -m32mscoff -ofbenchmark32mscoff.exe benchmark.d
+@if ERRORLEVEL 1 goto :eof
+benchmark32mscoff.exe
+
+dmd -O -inline -release -boundscheck=off -m64 -ofbenchmark64.exe benchmark.d
+@if ERRORLEVEL 1 goto :eof
+benchmark64.exe
+
+
+echo done...
+
+pause 
+goto :eof
+-----------------------------------
+DMD32 D Compiler v2.071.1
+Copyright (c) 1999-2015 by Digital Mars written by Walter Bright
+
+Documentation: http://dlang.org/
+Config file: C:\D\dmd.2.071.1\windows\bin\sc.ini
+Usage:
+  dmd files.d ... { -switch }
+
+  files.d        D source files
+  @cmdfile       read arguments from cmdfile
+  -allinst       generate code for all template instantiations
+  -betterC       omit generating some runtime information and helper functions
+  -boundscheck=[on|safeonly|off]   bounds checks on, in @safe only, or off
+  -c             do not link
+  -color[=on|off]   force colored console output on or off
+  -conf=path     use config file at path
+  -cov           do code coverage analysis
+  -cov=nnn       require at least nnn% code coverage
+  -D             generate documentation
+  -Dddocdir      write documentation file to docdir directory
+  -Dffilename    write documentation file to filename
+  -d             silently allow deprecated features
+  -dw            show use of deprecated features as warnings (default)
+  -de            show use of deprecated features as errors (halt compilation)
+  -debug         compile in debug code
+  -debug=level   compile in debug code <= level
+  -debug=ident   compile in debug code identified by ident
+  -debuglib=name    set symbolic debug library to name
+  -defaultlib=name  set default library to name
+  -deps          print module dependencies (imports/file/version/debug/lib)
+  -deps=filename write module dependencies to filename (only imports)
+  -dip25         implement http://wiki.dlang.org/DIP25 (experimental)
+  -g             add symbolic debug info
+  -gc            add symbolic debug info, optimize for non D debuggers
+  -gs            always emit stack frame
+  -gx            add stack stomp code
+  -H             generate 'header' file
+  -Hddirectory   write 'header' file to directory
+  -Hffilename    write 'header' file to filename
+  --help         print help and exit
+  -Ipath         where to look for imports
+  -ignore        ignore unsupported pragmas
+  -inline        do function inlining
+  -Jpath         where to look for string imports
+  -Llinkerflag   pass linkerflag to link
+  -lib           generate library rather than object files
+  -m32           generate 32 bit code
+  -m32mscoff     generate 32 bit code and write MS-COFF object files
+  -m64           generate 64 bit code
+  -main          add default main() (e.g. for unittesting)
+  -man           open web browser on manual page
+  -map           generate linker .map file
+  -noboundscheck no array bounds checking (deprecated, use -boundscheck=off)
+  -O             optimize
+  -o-            do not write object file
+  -odobjdir      write object & library files to directory objdir
+  -offilename    name output file to filename
+  -op            preserve source path for output files
+  -profile       profile runtime performance of generated code
+  -profile=gc    profile runtime allocations
+  -release       compile release version
+  -run srcfile args...   run resulting program, passing args
+  -shared        generate shared library (DLL)
+  -transition=id help with language change identified by 'id'
+  -transition=?  list all language changes
+  -unittest      compile in unit tests
+  -v             verbose
+  -vcolumns      print character (column) numbers in diagnostics
+  -verrors=num   limit the number of error messages (0 means unlimited)
+  -vgc           list all gc allocations including hidden ones
+  -vtls          list all variables going into thread local storage
+  --version      print compiler version and exit
+  -version=level compile in version code >= level
+  -version=ident compile in version code identified by ident
+  -w             warnings as errors (compilation will halt)
+  -wi            warnings as messages (compilation will continue)
+  -X             generate JSON file
+  -Xffilename    write JSON file to filename
+
--- benchmark_dmd_ldc/benchmark.d
+++ benchmark_dmd_ldc/benchmark.d
@@ -39,24 +39,24 @@ double tarai(double x, double y, double z)
 	if (x <= y) {
 		return z;
 	} else {
-		return tarai( tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y) );
+		return tarai( tarai(x - 1.0, y, z), tarai(y - 1.0, z, x), tarai(z - 1.0, x, y) );
 	}
 }
 
-void benchMark(void delegate() dg, string s)
+void benchMark_org(string title, void delegate() dg)
 {
 	import std.stdio : writeln;
 	import std.datetime : Clock;
 	import std.algorithm.sorting : sort;
 	
 	long[] sw;
-	writeln("# ", s, " bench ----");
+	writeln("# ", title, " bench ----");
 	foreach (i; 0 .. 10) {
-		long start = Clock.currStdTime() / 10000;
+		long start = Clock.currStdTime() / 10_000;
 		//fib(40);
 		//taraid(22.0, 11.0, 0);
 		dg();
-		long end = Clock.currStdTime() / 10000;
+		long end = Clock.currStdTime() / 10_000;
 		long result = end - start;
 		sw ~= result;
 		writeln(i, ": ", result);
@@ -73,6 +73,129 @@ void benchMark(void delegate() dg, string s)
 	
 }
 
+void benchMark_old(string title, void delegate() dg)
+{
+	import std.conv;
+	import core.time;
+//	import core.time : TickDuration, FracSec, msecs;
+	import std.datetime : StopWatch;
+	import std.algorithm.sorting : sort;
+	import std.stdio : writeln;
+	
+	StopWatch timer;
+	long[] tarray;
+	
+	writeln("# ", title, " ", buildID(), " bench ----");
+	foreach (i; 0 .. 10) {
+		timer.start();
+		//fib(40);
+		//taraid(22.0, 11.0, 0);
+		dg();
+		timer.stop();
+		long result = timer.peek().to!("msecs", long);
+		tarray ~= result;
+		writeln(i, ": ", result);
+		timer.reset();
+	}
+	sort(tarray);
+	writeln("1st: ", tarray[0], "ms");
+	writeln("wst: ", tarray[$-1], "ms");
+	
+	long total;
+	foreach (v; tarray) {
+		total += v;
+	}
+	writeln("ave: ", total / tarray.length, "ms");
+}
+
+
+
+// class Collect(T)
+struct Collect(T)
+{
+	private T[] array;
+
+	void add(in T v) {
+		array ~= v;
+	}
+	void sort() {
+		import std.algorithm.sorting : sort, isSorted;
+		if (!isSorted(array))
+			sort(array);
+	}
+	auto fastest() {
+		sort();
+		return array[0];
+	}
+	auto worst() {
+		sort();
+		return array[$-1];
+	}
+	auto average() {
+		T total;
+		foreach (v; array) {
+			total += v;
+		}
+		return total / array.length;
+	}
+}
+
+void benchMark_00(string title, void delegate() dg) {
+	import std.conv;
+	import core.time;
+	import std.datetime : StopWatch; //  ,TickDuration, msecs;
+	import std.stdio : writeln;
+	
+	StopWatch timer;
+	// 	auto c = new Collect!(long);
+	Collect!(long) c;
+	writeln("# ", title, " ", buildID(), " bench ----");
+	foreach (i; 0 .. 10) {
+		timer.start();
+		dg();
+		timer.stop();
+		long result = timer.peek().to!("msecs", long)();
+		c.add(result);
+		writeln(i, ": ", result);
+		timer.reset();
+	}
+	writeln("fastest: ", c.fastest(), "ms");
+	writeln("worst  : ", c.worst(), "ms");
+	writeln("average: ", c.average(), "ms");
+}
+
+
+string buildID()
+{
+	string s;
+	
+	version(DigitalMars) {
+		s ~= "DigitalMars";
+	} else version (LDC) {
+		s ~= "LDC";
+	} else version(GDC) {
+		s ~= "GDC";
+	} else {
+		s ~= "n/a";
+	}
+	s ~= "/";
+	version(Win32) {
+		s ~= "Win32";
+	} else version(Win64) {
+		s ~= "Win64";
+	} else {
+		s ~= "Win??";
+	}
+	s ~= "/";
+	version(CRuntime_DigitalMars) {
+		s ~= "CRuntime_DigitalMars";
+	} 
+	version(CRuntime_Microsoft) {
+		s ~= "CRuntime_Microsoft";
+	}
+	return s;
+}
+
 int main()
 {
 	void taki() {
@@ -81,8 +204,7 @@ int main()
 	void takd() {
 		tarai(22.0, 11.0, 0.0);
 	}
-	
-	benchMark(&taki, "tarai int");
-	benchMark(&takd, "tarai duble");
+	benchMark_00("tarai int", &taki);
+	benchMark_00("tarai duble", &takd);
 	return 0;
 }
